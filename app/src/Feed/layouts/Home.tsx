cb625a72bee77d121447393190db504b84610fe1
soulsam480 <soulsam480@hotmail.com>
2021-08-07 00:41:20 +0530

    feat(app): tweak the input lib component

--- app/src/Feed/layouts/Home.tsx
+++ app/src/Feed/layouts/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import BottomNav from 'src/Feed/components/BottomNav';
 import LeftNav from 'src/Feed/components/LeftNav';
 import RightNav from 'src/Feed/components/RightNav';
 import 'src/Feed/styles/layouts.scss';
+import JInput from 'src/Lib/JInput';
 import { classNames } from 'src/utils/helpers';
 import { useHideOnScroll } from 'src/utils/hooks';
 
@@ -11,13 +12,22 @@ interface Props {}
 
 const Authorized: React.FC<Props> = () => {
   const isHidden = useHideOnScroll();
-
+  const [val, setVal] = useState('');
   return (
     <div className="j-layout">
       <aside className="j-layout__leftbar">
         <LeftNav />
       </aside>
       <main className="j-layout__content">
+        <div className="pb-3">
+          <JInput
+            value={val}
+            onInput={(e) => setVal(e)}
+            placeholder="Add post content"
+            is="textarea"
+            className="h-52"
+          />
+        </div>
         <Outlet />
       </main>
       <aside className="j-layout__rightbar">
--- app/src/Lib/JInput.tsx
+++ app/src/Lib/JInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { classNames } from 'src/utils/helpers';
 
 interface Props {
@@ -8,18 +8,43 @@ interface Props {
   className?: string;
   id?: string;
   placeholder?: string;
+  is?: 'input' | 'textarea';
+  contentClass?: string;
 }
 
-const JInput: React.FC<Props> = ({ value, onInput, type, className, id, placeholder }) => {
+const JInput: React.FC<Props> = ({
+  value,
+  onInput,
+  type,
+  className,
+  id,
+  placeholder,
+  is,
+  contentClass,
+}) => {
+  is = !!is ? is : 'input';
+
   return (
-    <input
-      onChange={(e) => onInput(e.target.value)}
-      id={id}
-      type={type || 'text'}
-      className={classNames(['j-input', `${className ?? ''}`])}
-      value={value}
-      placeholder={placeholder}
-    />
+    <div className={classNames(['j-input', className || ''])}>
+      {is === 'input' ? (
+        <input
+          onChange={(e) => onInput(e.target.value)}
+          id={id}
+          type={type || 'text'}
+          className={classNames([`${contentClass || ''}`])}
+          value={value}
+          placeholder={placeholder}
+        />
+      ) : (
+        <textarea
+          onChange={(e) => onInput(e.target.value)}
+          id={id}
+          className={classNames([`${contentClass || ''}`])}
+          value={value}
+          placeholder={placeholder}
+        />
+      )}
+    </div>
   );
 };
 
