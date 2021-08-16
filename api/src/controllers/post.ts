7e3f9f00fe65f91a2353804e5ebb5155cf870a43
soulsam480 <soulsam480@hotmail.com>
2021-08-16 23:32:34 +0530

    refactor(api): post controller for better readability

--- api/src/controllers/post.ts
+++ api/src/controllers/post.ts
@@ -25,69 +25,75 @@ import { createError, formatResponse } from 'src/utils/helpers';
 //   res.send('OK');
 // });
 
-const postController = createController(
-  '/posts',
-  [
-    createRoute<any, any, { cursor: string; limit: string }>({
-      path: '/',
-      method: 'get',
-      handler: async (req, res, _, __, { limit: pagination_limit, cursor: pagination_cursor }) => {
-        try {
-          const cursor: number = parseInt(pagination_cursor);
-          const limit: number = parseInt(pagination_limit) || 10;
-          const data = await getAllPosts(cursor, limit);
-          res.json({
-            ...data,
-          });
-        } catch (error) {
-          console.log(error);
-          res.status(500).send(createError('Internal server error.', error));
-        }
-      },
-    }),
-    createRoute<any, { id: string }>({
-      path: '/user/:id',
-      method: 'get',
-      handler: async (req, res, _, { id }) => {
-        if (!id) return res.status(400).send(createError('User id not found'));
-        try {
-          const posts = await getPostsByUserId(id);
-          res.send(formatResponse(posts));
-        } catch (error) {
-          console.log(error);
-          res.status(500).send(createError('Internal server error.', error));
-        }
-      },
-    }),
-    createRoute<{ post: { [x: string]: any } }>({
-      path: '/',
-      method: 'post',
-      handler: async ({ userId }, res, { post }) => {
-        if (!Object.keys(post).length) res.status(400).send(createError('Post not found'));
-        try {
-          const newPost = await createPost({ ...(post as any), user: userId });
-          res.send(formatResponse(newPost));
-        } catch (error) {
-          console.log(error);
-          res.status(500).send(createError('Internal server error.', error));
-        }
-      },
-    }),
-    createRoute<any, { id: string }>({
-      path: '/:id/react',
-      method: 'post',
-      handler: async ({ userId }, res, _, { id }) => {
-        try {
-          const data = await reactPost(id, userId as string);
-          res.send(formatResponse(data));
-        } catch (error) {
-          console.log(error);
-          res.status(500).send(createError('Internal server error.', error));
-        }
-      },
-    }),
-  ],
-  [auth],
-);
+const getPosts = createRoute<any, any, { cursor: string; limit: string }>({
+  path: '/',
+  method: 'get',
+  handler: async (req, res, _, __, { limit: pagination_limit, cursor: pagination_cursor }) => {
+    try {
+      const cursor: number = parseInt(pagination_cursor);
+      const limit: number = parseInt(pagination_limit) || 10;
+
+      const data = await getAllPosts(cursor, limit);
+
+      res.json({
+        ...data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(createError('Internal server error.', error));
+    }
+  },
+});
+
+const postsByUserId = createRoute<any, { id: string }>({
+  path: '/user/:id',
+  method: 'get',
+  handler: async (req, res, _, { id }) => {
+    if (!id) return res.status(400).send(createError('User id not found'));
+
+    try {
+      const posts = await getPostsByUserId(id);
+
+      res.send(formatResponse(posts));
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(createError('Internal server error.', error));
+    }
+  },
+});
+
+const create = createRoute<{ post: { [x: string]: any } }>({
+  path: '/',
+  method: 'post',
+  handler: async ({ userId }, res, { post }) => {
+    if (!Object.keys(post).length) res.status(400).send(createError('Post not found'));
+
+    try {
+      const newPost = await createPost({ ...(post as any), user: userId });
+
+      res.send(formatResponse(newPost));
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(createError('Internal server error.', error));
+    }
+  },
+});
+
+const react = createRoute<any, { id: string }>({
+  path: '/:id/react',
+  method: 'post',
+  handler: async ({ userId }, res, _, { id }) => {
+    try {
+      const data = await reactPost(id, userId as string);
+
+      res.send(formatResponse(data));
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(createError('Internal server error.', error));
+    }
+  },
+});
+
+const postController = createController('/posts', [getPosts, postsByUserId, create, react], [auth]);
 
 export { postController };
