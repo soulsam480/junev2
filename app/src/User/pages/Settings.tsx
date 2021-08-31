c484eea332459eda0e5ade50b8068e92ca3a2a09
soulsam480 <soulsam480@hotmail.com>
2021-08-31 15:37:46 +0530

    feat: minor UI changes

--- app/src/User/pages/Settings.tsx
+++ app/src/User/pages/Settings.tsx
@@ -1,71 +1,110 @@
-import React, { useRef, useState } from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
 import JAvatar from 'src/Lib/JAvatar';
 import JButton from 'src/Lib/JButton';
-import JImage from 'src/Lib/JImage';
+// import JImage from 'src/Lib/JImage';
 import JInput from 'src/Lib/JInput';
-import { useUserStore } from '../store/useUserStore';
+import { useUserStore } from 'src/User/store/useUserStore';
 
-interface Props { }
+interface Props {}
 interface UserDetails {
-    name: string;
-    email: string;
-    bio?: string;
-    image?: string;
-    password: string;
+  name: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  password: string;
 }
 
 const Settings: React.FC<Props> = () => {
-    const { user } = useUserStore();
-    const inputFile = useRef<HTMLInputElement>(null);
-    const [userDetails, setUserDetails] = useState<UserDetails>({
-        name: user.name,
-        email: user.email,
-        bio: user.bio,
-        image: user.image,
-        password: ""
-    })
+  const user = useUserStore((state) => state.user);
 
-    const updateUserDetails = (e) => {
-e.prev
-    }
-    return (<>
-        <h1 className="text-center mb-4 font-bold text-2xl">User Settings</h1>
-        <form className="flex flex-col space-y-8" onSubmit={updateUserDetails}>
-            <span className="flex flex-col items-center justify-center ">
-                <JAvatar src="https://cdn.quasar.dev/img/boy-avatar.png" size="150px" rounded />
-                <JButton noBg icon="ion:camera-outline" size="25px"
-                    onClick={() => {
-                        inputFile.current?.click()
-                    }} />
-                <input type='file' id='file' ref={inputFile} style={{ display: 'none' }} />
-            </span>
-            <span className="relative">
-                <label htmlFor="name" className="absolute left-3 -top-3 bg-green-200 px-1">Name</label>
-                <JInput id="name" value={userDetails.name} onInput={(name) => setUserDetails({ ...userDetails, name })} type="text"
-                    className="border-2 border-green-500" is="input" />
-            </span>
+  const inputFile = useRef<HTMLInputElement>(null);
 
-            <span className="relative">
-                <label htmlFor="email" className="absolute left-3 -top-3 bg-green-200 px-1">Email</label>
-                <JInput id="email" value={userDetails.email} onInput={(email) => setUserDetails({ ...userDetails, email })} type="text"
-                    className="border-2 border-green-500" is="input" />
-            </span>
+  const [userDetails, setUserDetails] = useState<UserDetails>({
+    name: user.name,
+    email: user.email,
+    bio: user.bio,
+    image: user.image,
+    password: '',
+  });
 
-            <span className="relative">
-                <label htmlFor="bio" className="absolute left-3 -top-3 bg-green-200 px-1">Bio</label>
-                <JInput id="bio" value={userDetails.bio} onInput={(bio) => setUserDetails({ ...userDetails, bio })} type="text"
-                    className="border-2 border-green-500" is="textarea" />
-            </span>
+  const updateUserDetails = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <form className="flex flex-col space-y-8" onSubmit={updateUserDetails}>
+        <span className="flex flex-col items-center justify-center ">
+          <div className="py-2">
+            <JAvatar src="https://cdn.quasar.dev/img/boy-avatar.png" size="150px" rounded />
+          </div>
+          <JButton
+            icon="ion:camera-outline"
+            onClick={() => {
+              inputFile.current?.click();
+            }}
+            sm
+            flat
+            round
+            size="20px"
+          />
+          <input type="file" id="file" ref={inputFile} style={{ display: 'none' }} />
+        </span>
+        <span className="relative">
+          <label htmlFor="name" className="absolute left-3 -top-1 px-1 text-xs">
+            Name
+          </label>
+          <JInput
+            id="name"
+            value={userDetails.name}
+            onInput={(name) => setUserDetails({ ...userDetails, name })}
+            type="text"
+            is="input"
+          />
+        </span>
 
-            <span className="relative">
-                <label htmlFor="password" className="absolute left-3 -top-3 bg-green-200 px-1">Password</label>
-                <JInput id="password" value={userDetails.password} onInput={(password) => setUserDetails({ ...userDetails, password })} type="password"
-                    className="border-2 border-green-500" is="input" />
-            </span>
-            <JButton type="submit">Update</JButton>
-        </form>
+        <span className="relative">
+          <label htmlFor="email" className="absolute left-3 -top-1 px-1 text-xs">
+            Email
+          </label>
+          <JInput
+            id="email"
+            value={userDetails.email}
+            onInput={(email) => setUserDetails({ ...userDetails, email })}
+            type="text"
+            is="input"
+          />
+        </span>
+
+        <span className="relative">
+          <label htmlFor="bio" className="absolute left-3 -top-1 px-1 text-xs">
+            Bio
+          </label>
+          <JInput
+            id="bio"
+            value={userDetails.bio}
+            onInput={(bio) => setUserDetails({ ...userDetails, bio })}
+            type="text"
+            is="textarea"
+          />
+        </span>
+
+        <span className="relative">
+          <label htmlFor="password" className="absolute left-3 -top-1 px-1 text-xs">
+            Password
+          </label>
+          <JInput
+            id="password"
+            value={userDetails.password}
+            onInput={(password) => setUserDetails({ ...userDetails, password })}
+            type="password"
+            is="input"
+          />
+        </span>
+        <div className="flex justify-end">
+          <JButton type="submit">Update</JButton>
+        </div>
+      </form>
     </>
-
-    );
+  );
 };
 export default Settings;
